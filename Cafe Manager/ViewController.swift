3b108d54ef7e652fd784b0b788a04c8ccadbb0b3
Heshan <heshanmarasinghe@gmail.com>
2021-04-15 22:23:07 -0700

    Initial Commit

---
+++ Cafe Manager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cafe Manager
+//
+//  Created by Heshan on 4/15/21.
+//  Copyright © 2021 Heshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
